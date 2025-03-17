41d2013c3bc7c7fb84711ada948f29f31d4ab5e2
0 <ghostoftheshell@mailbox.org>
2025-03-17 19:05:45 +0000

    Move deliverables to build dir

--- build.sh
+++ build.sh
@@ -3,12 +3,17 @@
 CC=${CC:=gcc}
 SRCS="ds/array.c parsers/dotenv.c"
 OBJS=""
-LIBNAME=libcodex.a
+LIBNAME=build/libcodex.a
 
 set -x
 
+mkdir -p build
+mkdir -p build/ds
+mkdir -p build/testing
+mkdir -p build/parsers
+
 for src in $SRCS; do
-	obj="${src%.c}.o"
+	obj="build/${src%.c}.o"
 	gcc -c "$src" -o "$obj"
 	OBJS="$OBJS $obj"
 done
