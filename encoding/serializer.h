db83a736e825a062eff551dbb371da6fe7712a53
0 <ghostoftheshell@mailbox.org>
2025-03-19 15:27:29 +0000

    Move string serialization/deserialization to separate functions

--- encoding/serializer.h
+++ encoding/serializer.h
@@ -1,6 +1,10 @@
 #ifndef CODEX_SERIALIZER_H_
 #define CODEX_SERIALIZER_H_
+
+#include <endian.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*interface*/ typedef struct Serializer {
 	void *ctx;
@@ -11,5 +15,38 @@
 
 	void   (*release)          (struct Serializer **pself);
 } Serializer;
+
+static size_t
+serialize_string(char *s, uint8_t *buffer) {
+	uint8_t *p = buffer;
+	size_t size = 0;
+	uint64_t len = strlen(s) + 1;
+	uint64_t belen = htobe64(len);
+	memcpy(p, &belen, sizeof(belen));
+	p += sizeof(belen);
+	size += sizeof(belen);
+
+	memcpy(p, s, len);
+	size += len;
+
+	return size;
+}
+
+static size_t
+deserialize_string(uint8_t *buffer, char **s) {
+	uint8_t *p = buffer;
+	size_t size = 0;
+	uint64_t len;
+	memcpy(&len, p, sizeof(len));
+	p += sizeof(len);
+	size += sizeof(len);
+
+	len = be64toh(len);
+	*s = malloc(len);
+	memcpy(*s, p, len);
+	size += len;
+
+	return size;
+}
 
 #endif
--- tests/_07_array_serializer.c
+++ tests/_07_array_serializer.c
@@ -16,8 +16,8 @@ static size_t
 emulacrum_tarot_serializer_estimate_size(Serializer *s, void *entity) {
 	EmulacrumTarot *et = (EmulacrumTarot *) entity;
 	return sizeof(et->number) + 
-	       sizeof(uint32_t) + strlen(et->original) + 1 + 
-	       sizeof(uint32_t) + strlen(et->emulacrum) + 1; 
+	       sizeof(uint64_t) + strlen(et->original) + 1 + 
+	       sizeof(uint64_t) + strlen(et->emulacrum) + 1; 
 }
 
 static size_t
@@ -31,23 +31,13 @@ emulacrum_tarot_serializer_serialize_to(Serializer *s, void *entity, uint8_t *bu
 	p += sizeof(number);
 	size += sizeof(number);
 
-	uint32_t len = strlen(et->original) + 1;
-	uint32_t belen = htobe32(len);
-	memcpy(p, &belen, sizeof(belen));
-	p += sizeof(len);
-	size += sizeof(len);
-	memcpy(p, et->original, len);
-	p += len;
-	size += len;
+	size_t i = serialize_string(et->original, p);
+	p += i;
+	size += i;
 
-	len = strlen(et->emulacrum) + 1;
-	belen = htobe32(len);
-	memcpy(p, &belen, sizeof(belen));
-	p += sizeof(len);
-	size += sizeof(len);
-	memcpy(p, et->emulacrum, len);
-	p += len;
-	size += len;
+	i = serialize_string(et->emulacrum, p);
+	p += i;
+	size += i;
 
 	return size;
 }
@@ -63,26 +53,13 @@ emulacrum_tarot_serializer_deserialize_from(Serializer *s, uint8_t *buffer, void
 	p += sizeof(et->number);
 	size += sizeof(et->number);
 
-	uint32_t len;
-	memcpy(&len, p, sizeof(len));
-	p += sizeof(len);
-	size += sizeof(len);
+	size_t i = deserialize_string(p, &et->original);
+	p += i;
+	size += i;
 
-	len = be32toh(len);
-	et->original = malloc(len);
-	memcpy(et->original, p, len);
-	p += len;
-	size += len;
-
-	memcpy(&len, p, sizeof(len));
-	p += sizeof(len);
-	size += sizeof(len);
-
-	len = be32toh(len);
-	et->emulacrum = malloc(len);
-	memcpy(et->emulacrum, p, len);
-	p += len;
-	size += len;
+	i = deserialize_string(p, &et->emulacrum);
+	p += i;
+	size += i;
 
 	*entity = et;
 
