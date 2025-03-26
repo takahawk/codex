295628b95097a93fbe2c697257d99f29720f20b1
0 <ghostoftheshell@mailbox.org>
2025-03-26 13:44:09 +0000

    RBTree: implement iterator

--- ds/rbtree_iter_internal.h
+++ ds/rbtree_iter_internal.h
@@ -3,66 +3,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef enum {
-	NOT_VISITED    = 0,
-	LEFT_VISITED   = 1,
-	COMPLETED      = 2
-} RBTreeIteratorState;
-
 typedef struct {
 	Allocator           *a;
-	RBTreeIteratorState state;
 	RBTreeNode          *node;
 } RBTreeIteratorCtx;
 
 static bool
 rbtree_iterator_has_next(Iterator *self) {
 	RBTreeIteratorCtx *ctx = self->ctx;
-	RBTreeIteratorState state = ctx->state;
-	return state != COMPLETED;
+	return NULL != ctx->node;
+}
+
+static void
+find_next(Iterator *self) {
+	RBTreeIteratorCtx *ctx = self->ctx;
+	RBTreeNode *node = ctx->node;
+
+	if (node == NULL) {
+		fprintf(stderr, "there is no next element\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (node->right) {
+		node = node->right;
+		while (node->left) {
+			node = node->left;
+		}
+		ctx->node = node;
+		return;
+	} 
+
+	while (node->p != NULL && node->p->right == node) {
+		node = node->p;
+	}
+
+	if (NULL == node->p) {
+		ctx->node = NULL;
+		return;
+	}
+
+	// node->p->left == node
+	ctx->node = node->p;
+	return;
+
 }
 
 static void*
 rbtree_iterator_next(Iterator *self) {
 	RBTreeIteratorCtx *ctx = self->ctx;
-	RBTreeIteratorState state = ctx->state;
 	RBTreeNode *node = ctx->node;
-	if (state == COMPLETED) {
+	if (NULL == node) {
 		fprintf(stderr, "there is no next element\n");
 		exit(EXIT_FAILURE);
 	}
 
-	if (state == NOT_VISITED) {
-		while (NULL != node->left) {
-			node = node->left;
-		}
-
-		ctx->node = node->p;
-		ctx->state = LEFT_VISITED;
-		return node;
-	}
-
-	if (state == LEFT_VISITED) {
-		RBTreeNode *returnNode = node;
-		// find next at once
-		if (NULL == node->right) {
-			while (NULL != node->p && node->p->right == node) {
-				node = node->p;
-			}
-			if (NULL == node->p) {
-				ctx->state = COMPLETED;
-			} else {
-				// node was left child
-				ctx->node = node;
-				ctx->state = LEFT_VISITED;
-			}
-		} else {
-			state = NOT_VISITED;
-			ctx->node = node->right;
-		}
-		return returnNode;
-	}
-
+	find_next(self);
+	return node;
 }
 
 static void
@@ -88,8 +84,11 @@ rb_tree_form_iterator(RBTree *self) {
 
 	RBTreeIteratorCtx *ctx = a->alloc(a, sizeof(RBTreeIteratorCtx));
 	ctx->a = a;
-	ctx->state = self->root == NULL ? COMPLETED : NOT_VISITED;
 	ctx->node = self->root;
+	if (NULL != ctx->node) {
+		while (ctx->node->left)
+			ctx->node = ctx->node->left;
+	}
 
 	Iterator *i = a->alloc(a, sizeof(Iterator));
 	*i = RBTREE_ITER_PROTOTYPE;
---
+++ tests/_11_rb_tree_iterate.c
@@ -0,0 +1,113 @@
+#include "codex/allocators/debug_allocator.h"
+#include "codex/ds/comparator.h"
+#include "codex/ds/rbtree.h"
+
+#include "codex/testing/assert.h"
+
+
+int main() {
+	Allocator *a = form_debug_allocator(&std_allocator);
+
+	RBTree/*char**/ *rb = form_rbtree(a, str_comparator);
+
+	size_t word_count = 40;
+	char words[][16] = {
+	    "Emulacrum",
+	    "Zero",
+	    "Reflexia",
+	    "TheMainframe",
+	    "Subworld",
+	    "Simulacrum",
+	    "RootAccess",
+	    "Anamnesis",
+	    "VoidEcho",
+	    "ShadowRealm",
+	    "4by55",
+	    "WuWei",
+	    "FractalSelf",
+	    "DreamLink",
+	    "Backdoor",
+	    "PsyShift",
+	    "Karma",
+	    "Kriya",
+	    "Prana",
+	    "TheMind",
+	    "TheVoice",
+	    "TheEye",
+	    "TheSoul",
+	    "Overworld",
+	    "SubSim",
+	    "SupraSim",
+	    "TheGreatNet",
+	    "UnrealReal",
+	    "Anatta",
+	    "Samsara",
+	    "Maya",
+	    "Monad",
+	    "Emanation",
+	    "TrueName",
+	    "PhaseState",
+	    "ZeroState",
+	    "RootVoid",
+	    "Sigil",
+	    "Tathata",
+	    "NoSelf"
+	};
+
+	char sorted_words[][16] = {
+	    "4by55",
+	    "Anamnesis",
+	    "Anatta",
+	    "Backdoor",
+	    "DreamLink",
+	    "Emanation",
+	    "Emulacrum",
+	    "FractalSelf",
+	    "Karma",
+	    "Kriya",
+	    "Maya",
+	    "Monad",
+	    "NoSelf",
+	    "Overworld",
+	    "PhaseState",
+	    "Prana",
+	    "PsyShift",
+	    "Reflexia",
+	    "RootAccess",
+	    "RootVoid",
+	    "Samsara",
+	    "ShadowRealm",
+	    "Sigil",
+	    "Simulacrum",
+	    "SubSim",
+	    "Subworld",
+	    "SupraSim",
+	    "Tathata",
+	    "TheEye",
+	    "TheGreatNet",
+	    "TheMainframe",
+	    "TheMind",
+	    "TheSoul",
+	    "TheVoice",
+	    "TrueName",
+	    "UnrealReal",
+	    "VoidEcho",
+	    "WuWei",
+	    "Zero",
+	    "ZeroState"
+	};
+	
+	for (size_t i = 0; i < word_count; ++i) {
+		rb->set(rb, words[i], NULL);
+	}
+
+	size_t i = 0;
+	Iterator *it = rb->form_iterator(rb);
+	while (it->has_next(it)) {
+		RBTreeNode *node = it->next(it);
+
+		assert_str_equals(node->key, sorted_words[i++]);
+	}
+	// TODO: memory check
+	return 0;
+}
